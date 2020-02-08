8c575f8113ee018cdb1e3b182cfcd1b0d692ccf5
Larry Y <larry1567@gmail.com>
2020-02-07 21:29:53 -0500

    Added feature "FIXME Log"

--- Project01/README.md
+++ Project01/README.md
@@ -1,4 +1,13 @@
 # CS 1XA3 Project01 - yaol13
 
 ## Usage
-	
+    Execute this script from the Project01 folder with:
+        chmod +x project_analyze.sh
+        ./project_analyze.sh
+    Once the script runs, you will be prompted to input the feature you want to use.
+    Simply enter in the name of the feature (listed in each feature's section below) you wish to run, then press enter.
+
+## Checkout Latest Merge
+    Description: This feature will locate the latest feature with the word "merge" in its commit message, 
+        and then automatically checkout that commit. Note: This will put you in a detached HEAD state.
+    Execution: Input "checkout_latest_merge" (without the quotes) when prompted.
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-featHello() {
-	echo "Hello there"
-}
+# Find the path to the root directory of this repo
+repoRoot=$(git rev-parse --show-toplevel)
+#echo "Repo root is $repoRoot"
 
 checkout_latest_merge() {
-	#TODO
 	latestMerge=$(git log --oneline | grep -i "merge" | head -1 | cut -d " " -f1 )
 	git checkout $latestMerge
 	echo "Checked out commit $latestMerge"
+}
+
+fixme_log() {
+	if [ -f "fixme.log" ] ; then
+		rm "fixme.log"
+	fi
+	touch "fixme.log"
+	fileList=$(find "$repoRoot" -type d -name '.git' -prune -o -type f -print) # from https://unix.stackexchange.com/a/350090
+	IFS=$'\n' # Filepaths might have spaces in them, which breaks the for loop
+	for file in $fileList ; do
+		lastLine=$(tail -n 1 "$file")
+		#echo $lastLine
+		#echo $file
+		if [ "$lastLine" == '#FIXME' ] ; then
+			#echo "I am doing something"
+			echo "$file" >> "./fixme.log"
+		fi
+		#echo "Done with this file"
+	done
+	unset IFS
+	echo "Generated \"CS1XA3/Project01/fixme.log\"."
 }
 
 echo "Enter name of feature to run:"
