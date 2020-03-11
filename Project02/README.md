5fc6cc3fdf615cb702b6ebdb483fd61d608025ff
Larry Y <larry1567@gmail.com>
2020-03-11 11:52:30 -0400

    Barebones CV with sections

---
+++ Project02/README.md
@@ -0,0 +1,29 @@
+# CS 1XA3 Project02 - yaol13
+
+## Overview
+
+This webpage is Larry Yao's custom CV.
+
+## Design
+
+**Description**: I made the following design choices ...
+
+## Custom Javascript Code -- Collapsable Sections
+
+**Description**: This feature allows the user to show or hide different sections of the CV, allowing me to describe
+different aspects in detail without making the webpage too cluttered. If an HTML element with the "view-toggle" class
+is clicked (usually a button), the script looks for any element that shares a parent with the button and has the "hide-click" class.
+If there is at least one visible element that satisfies those conditions, the script hides all visible elements that also satisfy those conditions.
+Otherwise, it displays them if they are hidden.
+
+## Custom Javascript Code -- Sticky Navigation Bar
+
+**Description**: This feature makes the webpage's navigation bar "stick" to the top of the window when the user scrolls past it,
+allowing them to easily navigate to different sections of the page. The script will also highlight the section that the user is currently
+browsing on the navigation bar.
+
+**Reference**: stickyNav() code was modified from [this w3schools tutorial](https://www.w3schools.com/howto/howto_js_navbar_sticky.asp) to use jQuery instead.
+
+## HTML/CSS References
+
+- This page uses W3.CSS from [this template](https://www.w3schools.com/w3css/tryw3css_templates_teal.htm).
---
+++ Project02/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+    // y-offset of the navbar
+    var yOffset = $("#navBar").offset().top;
+    // y-positions of sections relative to the document
+    var highlightOffsetVal = 128;
+    var yPosSkills = $("#section-skills").offset().top - highlightOffsetVal;
+    var yPosProjects = $("#section-projects").offset().top - highlightOffsetVal;
+    var yPosEducation = $("#section-education").offset().top - highlightOffsetVal;
+    var yPosExperience = $("#section-experience").offset().top - highlightOffsetVal;
+    // Last section of the CV
+    var lastSectionBtn = $("#btn-experience");
+
+    // Hide elements if visible, otherwise show them.
+    function toggleCollapser() {
+        if ($(this).parent().find(".hide-click:visible").length > 0) {
+            $(this).parent().find(".hide-click:visible").hide();
+            $(this).text(function () {
+                return "Expand";
+            });
+        }
+        else {
+            $(this).parent().find(".hide-click:hidden").show();
+            $(this).text(function () {
+                return "Collapse";
+            });
+        }
+        //console.log($(this).parent().find(".hide-click:visible"))
+        //console.log($(this).parent().find(".hide-click:hidden"))
+    }
+
+    // Sticky navbar
+    function stickyNav() {
+        if ($(window).scrollTop() >= yOffset) {
+            $("#navBar").addClass("navbar-sticky");
+        }
+        else {
+            $("#navBar").removeClass("navbar-sticky");
+        }
+    }
+
+    // Highlight buttons on navbar based on user's position in the document
+    function navHighlight() {
+        // Reset theme colours for all navbar items
+        $(".w3-bar-item").removeClass("w3-theme-l1");
+        $(".w3-bar-item").removeClass("w3-theme");
+        $(".w3-bar-item").addClass("w3-theme");
+
+        // Change colours based on scroll position in document
+        if ($(window).scrollTop() < yPosSkills) {
+            $("#btn-home").addClass("w3-theme-l1");
+            $("#btn-home").removeClass("w3-theme");
+        }
+        else if ($(window).scrollTop() < yPosProjects) {
+            $("#btn-skills").addClass("w3-theme-l1");
+            $("#btn-skills").removeClass("w3-theme");
+        }
+        else if ($(window).scrollTop() < yPosEducation) {
+            $("#btn-projects").addClass("w3-theme-l1");
+            $("#btn-projects").removeClass("w3-theme");
+        }
+        else if ($(window).scrollTop() < yPosExperience) {
+            $("#btn-education").addClass("w3-theme-l1");
+            $("#btn-education").removeClass("w3-theme");
+        }
+        else {
+            lastSectionBtn.addClass("w3-theme-l1");
+            lastSectionBtn.removeClass("w3-theme");
+        }
+    }
+
+    $(".view-toggle").click(toggleCollapser);
+    $(window).scroll(stickyNav);
+    $(window).scroll(navHighlight);
+});
